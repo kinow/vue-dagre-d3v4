78fd457117759cb0f32d7529eec4a5823952f66e
vdidonato <valentino.didonato@gmail.com>
2017-09-19 10:57:22 +0200

    switch to dagre-d3-webpack dependency

--- src/vue-dagre-d3v4.vue
+++ src/vue-dagre-d3v4.vue
@@ -1,10 +1,16 @@
 <script>
+import * as d3 from 'd3';
+import dagre from 'dagre-d3-webpack';
+
 export default {
   name: 'vue-dagre-d3v4',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  render(){
+
   }
 }
 </script>
